68cf8122d75e27080485ee98ea6770327e00c3cf
lwan1011@qq.com <aA5201314>
2019-04-04 11:41:54 +0800

    jjjjjffffff

---
+++ app/src/main/java/com/zc/shop/activity/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.zc.shop.activity;
+
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.zc.shop.R;
+import com.zc.shop.base.BaseActivity;
+import com.zc.shop.widget.NoScrollViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.login_viewpager)
+    protected NoScrollViewPager viewpager;
+
+    private List<Fragment> mFragments;
+
+    private Context con;
+
+    @BindView(R.id.login_tablayout)
+    protected TabLayout tablayout;
+    private String[] names = {"注册","登录"};
+
+    @Override
+    protected void init() {
+
+        viewpager.setScroll(false);
+
+        tablayout.setTabTextColors(ContextCompat.getColor(this, R.color.black), ContextCompat.getColor(this, R.color.red));
+        tablayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.red));
+
+        mFragments = new ArrayList<>();
+    }
+
+    @Override
+    protected int getContentResourseId() {
+        return R.layout.activity_login;
+    }
+}
---
+++ app/src/main/java/com/zc/shop/fragment/LoginFragment.java
@@ -0,0 +1,16 @@
+package com.zc.shop.fragment;
+
+import com.zc.shop.R;
+import com.zc.shop.base.BaseFragment;
+
+public class LoginFragment extends BaseFragment {
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getContentResourseId() {
+        return R.layout.login_fragment;
+    }
+}
