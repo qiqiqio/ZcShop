cc87700b5e7f5b32634a19d571b29101f4d7bfc6
lwan1011 <lwan1011@qq.com>
2019-04-05 00:14:06 +0800

    yyyyyyyyyyyyyyyyyy

---
+++ app/src/main/java/com/zc/shop/bean/Goods.java
@@ -0,0 +1,63 @@
+package com.zc.shop.bean;
+
+public class Goods {
+
+    private String id;
+
+    private String picUrl;
+
+    private String name;
+
+    private String price;
+
+    private String point;
+    private int height;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
--- app/src/main/java/com/zc/shop/fragment/item/ItemFragment.java
+++ app/src/main/java/com/zc/shop/fragment/item/ItemFragment.java
@@ -1,16 +1,17 @@
 package com.zc.shop.fragment.item;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.cjj.MaterialRefreshLayout;
 import com.zc.shop.R;
+import com.zc.shop.adapter.RecyclerAdapter;
 import com.zc.shop.base.BaseFragment;
+import com.zc.shop.bean.Goods;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -19,7 +20,7 @@ import butterknife.BindView;
  * Created by ylwx on 2018/8/5.
  */
 
-public class ItemFragment extends BaseFragment {
+public class ItemFragment extends BaseFragment implements RecyclerAdapter.OnItemClickListener {
 
     @BindView(R.id.recyclerview)
     RecyclerView mRecyclerView;
@@ -31,10 +32,49 @@ public class ItemFragment extends BaseFragment {
     @Override
     protected void init() {
 
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), 2);
+        //设置布局管理器
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        //设置为垂直布局，这也是默认的
+        gridLayoutManager.setOrientation(OrientationHelper.VERTICAL);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this.getContext(), getData());
+        mRecyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(this);//将接口传递到数据产生的地方
+
+    }
+
+    private List<Goods> getData() {
+        ArrayList<Goods> list =new ArrayList<Goods>();
+        Goods goods1=new Goods();
+        goods1.setName("商品1");
+
+        Goods goods2=new Goods();
+        goods1.setName("商品12");
+
+        Goods goods3=new Goods();
+        goods1.setName("商品13");
+
+        Goods goods4=new Goods();
+        goods1.setName("商品14");
+
+        list.add(goods1);
+        list.add(goods2);
+        list.add(goods3);
+        list.add(goods4);
+
+        return list;
+
     }
 
     @Override
     protected int getContentResourseId() {
         return R.layout.item_fragment;
+    }
+
+    @Override
+    public void onItemClick(int position, Goods model) {
+
     }
 }
