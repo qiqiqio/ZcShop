83dc46d7396fbad6df592466a01780229e8f528f
lwan1011@qq.com <aA5201314>
2019-04-09 18:23:43 +0800

    jjjjjffffff

---
+++ app/src/main/java/com/zc/shop/activity/GoodsInfoActivity.java
@@ -0,0 +1,17 @@
+package com.zc.shop.activity;
+
+import com.zc.shop.R;
+import com.zc.shop.base.BaseActivity;
+
+public class GoodsInfoActivity extends BaseActivity {
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected int getContentResourseId() {
+        return R.layout.activity_good_info;
+    }
+}
